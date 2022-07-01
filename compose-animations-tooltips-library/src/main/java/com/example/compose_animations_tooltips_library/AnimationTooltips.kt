2b37252b2cec5310bc698c4dc76691507258b47e
marcinkapcinski <mkapcinski@prograils.com>
2022-07-01 13:07:26 +0200

    Change description inside AnimationTooltips.kt

--- compose-animations-tooltips-library/src/main/java/com/example/compose_animations_tooltips_library/AnimationTooltips.kt
+++ compose-animations-tooltips-library/src/main/java/com/example/compose_animations_tooltips_library/AnimationTooltips.kt
@@ -24,14 +24,14 @@ import kotlinx.coroutines.delay
  * This function is used to easily create a series of animations describing subsequent functions in
  * the application.
  * The function consists of 5 phases:
-- phase 0: No new objects on the screen (state: 0)
-- phase 1: The appearance of a large circle from the first object on the tooltipsList: List <AnimationObject> (state: 1)
+- phase 0: No new objects on the screen (state: LOADING)
+- phase 1: The appearance of a large circle from the first object on the tooltipsList: List <AnimationObject> (state: SHOW_BIG_CIRCLE)
 - phase 2: during this phase, the animations included in the list start to appear, in order to
-           switch between animations it is required to overwrite the screen by the user (state: from 2 to tooltips.size+2)
-- phase 3: disappearance of the icon, description and small circle (state == -1)
-- phase 4: disappearing icon, description and small circle (state == -2)
-- phase 5: changing the color of the large circle  (state == -3)
-- phase 6: big circle disappearing animation (state == -4)
+           switch between animations it is required to overwrite the screen by the user (state: ANIMATIONS_PHASE)
+- phase 3: disappearance of the icon, description and small circle (state == DISAPPEAR_PHASE_1_HIDE_CONTENT)
+- phase 4: changing the color of the large circle  (state == DISAPPEAR_PHASE_2_CHANGE_BIG_CIRCLE_COLOR)
+- phase 5: big circle disappearing animation (state == DISAPPEAR_PHASE_3_HIDE_BIG_CIRCLE_SIZE)
+- phase 6: end of onboarding (state == FINISHED)
  */
 @SuppressLint("UnrememberedMutableState")
 @Composable
